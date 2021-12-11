5494f7e6678e32b50a77983651862231dba52d2b
vuong <levanvuong12022001@gmail.com>
2021-12-11 21:26:48 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Vượng Cute",
+    introTitle: 'Anh hỏi cái này',
+    introDesc: `Đây là một câu hỏi dễ thui.
+    Trẻ con cũng biết.
+    Không biết thì ngu si lắm`,
+    btnIntro: '^^HiHi^^',
+    title: 'Em có bị ngu si không?🤔',
+    desc: 'Dễ nhỉ?? Không biết nữa thì thui đấy. Trả lời nhanh nhanh lên xem nào!!! ',
+    btnYes: 'Có chứ 🙃',
+    btnNo: 'Không nha 🙂',
+    question: 'Haha. Có ý kiến gì không?? Ghi vào đây nè 👇',
+    btnReply: 'Gửi đi <3',
+    reply: 'Ơ. Em bị ngu thật mà. Không chỉ vậy còn bị ngáo nữa :)))))',
+    mess: 'Anh biết ngay mà 🤣. Không sao tuy em ngu si nhưng bù lại được cái xấu xí cơ mà 🙂',
+    messDesc: 'Vậy là quá xịn. Nhỉ ? ',
+    btnAccept: 'Okiiiii 🙆',
+    messLink: 'https://m.me/profile.php?id=100016032737446' //link mess của các bạn. VD: https://m.me/profile.php?id=100016032737446
+}
